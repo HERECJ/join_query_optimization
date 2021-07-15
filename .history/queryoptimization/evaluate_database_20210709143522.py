020436b143c902b68c78d0363a04e1007de1800e
chenjin <2014060201023@std.uestc.edu.cn>
2021-07-15 21:07:32 +0800

    modify execute

--- .history/queryoptimization/evaluate_database_20210709143522.py
+++
@@ -1,6 +0,0 @@
-import ray
-from ray.rllib.evaluation.metrics import collect_episodes
-from ray.rllib.utils.test_utils import check_learning_achieved
-
-def custom_eval_function(trainer, eval_workers):
-    worker_ = eval_workers.remote_workers()
--- .history/test_code_20210706151442.py
+++
@@ -1,6 +0,0 @@
-import torch
-import torch.nn as nn
-
-class MyModel(nn.Module):
-    def __init__(self):
-        super().__init__()
